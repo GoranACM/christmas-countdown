0d52c6b1c7cc9dca85f45f3402a30b1eed8d9934
GoranACM <goranacm@gmail.com>
2020-11-01 19:47:03 +1100

    Initial styling

--- src/App.js
+++ src/App.js
@@ -39,25 +39,36 @@ function App() {
     }
 
     timerComponents.push(
-      <span>
-        {timeLeft[interval]} {interval}{' '}
-      </span>
+      <div className='interval'>
+        <span>
+          {timeLeft[interval]} <br /> {interval}{' '}
+        </span>
+      </div>
     );
   });
 
   return (
-    <>
-      <div>
-        <h1>Christmas {year} countdown</h1>
-      </div>
-      <div>
+    <main>
+      {/* <div className='container'> */}
+      <svg viewBox='0 0 500 200'>
+        <path
+          id='curve'
+          d='M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97'
+        />
+        <text width='500'>
+          <textPath href='#curve'>Christmas {year} countdown</textPath>
+        </text>
+      </svg>
+      <div className='timer'>
         {timerComponents.length ? (
           timerComponents
         ) : (
           <span>MERRY CHRISTMAS!</span>
         )}
       </div>
-    </>
+      {/* <div className='title'><h1>Christmas {year} countdown</h1></div> */}
+      {/* </div> */}
+    </main>
   );
 }
 
